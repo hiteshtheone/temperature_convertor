9bef3529848387d2c70d192db128d960a3028e7a
hiteshtheone <hiteshtheone@gmail.com>
2015-03-22 19:15:18 +0530

    Initial Commit

---
+++ Temperature_convertor/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Temperature_convertor
+//
+//  Created by hambarkh on 22/03/15.
+//  Copyright (c) 2015 Akola. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
